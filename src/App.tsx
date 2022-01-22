4aba3e0ff31700e9772abf459be6f02abdf486ca
Victor Huang <victorhuangwq@gmail.com>
2022-01-22 12:37:02 -0500

    Add sponsors at bottom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Card } from './components/card';
 import './App.css';
+import { Sponsors } from './components/sponsors';
+
 
 export default function App() {
     return (
         <div className="App">
             <Card delay={50}/>
+            <Sponsors delay={10}/>            
         </div>
     );
 };
